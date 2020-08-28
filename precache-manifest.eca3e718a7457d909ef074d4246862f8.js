87713843e08fedb1f3866f25e10b36fc28ebca3d
kguzik <karolina.guzik08@gmail.com>
2020-08-28 17:26:58 +0200

    Updates

---
+++ precache-manifest.eca3e718a7457d909ef074d4246862f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca59323485686c0e729200497b488c64",
+    "url": "/movie-quotes-quiz/index.html"
+  },
+  {
+    "revision": "ba52b5fcebe6a6d97643",
+    "url": "/movie-quotes-quiz/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "04037f21d3aefa6e05e6",
+    "url": "/movie-quotes-quiz/static/css/main.c1fc0236.chunk.css"
+  },
+  {
+    "revision": "ba52b5fcebe6a6d97643",
+    "url": "/movie-quotes-quiz/static/js/2.674ecb63.chunk.js"
+  },
+  {
+    "revision": "d0976beec747e76a84c259bf2408c4cf",
+    "url": "/movie-quotes-quiz/static/js/2.674ecb63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04037f21d3aefa6e05e6",
+    "url": "/movie-quotes-quiz/static/js/main.f9c238b6.chunk.js"
+  },
+  {
+    "revision": "39c26d6305ceca88f494",
+    "url": "/movie-quotes-quiz/static/js/runtime-main.fb173c1c.js"
+  },
+  {
+    "revision": "3219419957b7d03578da90e20d8a7401",
+    "url": "/movie-quotes-quiz/static/media/button-sound.32194199.mp3"
+  },
+  {
+    "revision": "ee6aadae18d9da389dc08dd8d72e3721",
+    "url": "/movie-quotes-quiz/static/media/correct-answer.ee6aadae.mp3"
+  },
+  {
+    "revision": "c7106542fc4e8075bf1bae4bf6854692",
+    "url": "/movie-quotes-quiz/static/media/wrong-answer.c7106542.mp3"
+  }
+]);
