e0269433d2458048b99cad9594ec89f8b40cea06
kguzik <karolina.guzik08@gmail.com>
2020-09-01 16:07:12 +0200

    semicolon added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { Link } from 'react-router-dom';
-import {Container, Row} from 'react-bootstrap'
+import {Container, Row} from 'react-bootstrap';
 
 function Home(){
     return (
