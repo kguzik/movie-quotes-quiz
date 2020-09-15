aa923b96886ef527da63be4233e56faaa3063ab3
kguzik <karolina.guzik08@gmail.com>
2020-09-15 11:41:06 +0200

    strict mode removed

--- src/index.js
+++ src/index.js
@@ -5,9 +5,7 @@ import './styles/styles.scss';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
