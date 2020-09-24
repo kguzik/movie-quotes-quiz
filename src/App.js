6e4d3057d1884005f27969c3d707a45314ede3f8
kguzik <karolina.guzik08@gmail.com>
2020-09-24 13:57:26 +0200

    props to components Header and Footer added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
 import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import Icon from '@material-ui/core/Icon';
 import Home from './components/Home';
 import QuizInstructions from './components/quiz/QuizInstructions'
 import Quiz from './components/quiz/Quiz';
 import Summary from './components/quiz/Summary';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import firebase from './Config';
-
-function Header(){
-  return (
-    <div>
-      <div className="logo">
-        <Icon fontSize="large">movie</Icon>
-        <strong>Movie Quotes Quiz</strong>  
-      </div>
-    </div>
-  )
-}
-
-function Footer() {
-  return (
-    <div id="footer">
-      <p>Designed & created by Karolina Guzik</p>
-    </div>
-  )
-}
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function getQuestions() {
   const ref = firebase.firestore().collection('quotes');
@@ -37,14 +19,14 @@ function getQuestions() {
 function App() {
   return (
     <>
-      <Header/>
+      <Header iconName="movie" appName="Movie Quotes Quiz"/>
       <Router basename='/movie-quotes-quiz'>
         <Route path="/" exact component={Home}/>
         <Route path="/play/instruction" exact component={QuizInstructions}/>
         <Route path="/play/quiz" exact render={(props) => <Quiz getQuestions={getQuestions} {...props} />}/>
         <Route path="/play/summary" exact render={(props) => <Summary getQuestions={getQuestions} {...props} />}/>
       </Router>
-      <Footer/>
+      <Footer title="Designed &amp; created by Karolina Guzik"/>
     </>
   )
 }
